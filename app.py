d1440961c7a597afdda36f1433e96b4e1a44d268
Areeviik <arevik.khachaturian@gmail.com>
2025-07-16 10:34:51 +0400

    feat: Add Security Group Stack

--- app.py
+++ app.py
@@ -5,15 +5,25 @@ import aws_cdk as cdk
 from aws_cdk import Environment
 
 from shinecosucan_cdk.vpc import VpcStack
+from shinecosucan_cdk.security_group import SecurityGroupStack
 
 app = cdk.App()
 
 cidr = app.node.try_get_context("vpc_cidr") or os.getenv("VPC_CIDR") or "10.0.0.0/16"
 max_azs = app.node.try_get_context("max_azs") or int(os.getenv("MAX_AZS") or 2)
 
+# VPC Stack
 vpc_stack = VpcStack(app, "VpcStack",
 				cidr=cidr,
 				max_azs=max_azs,
+				env=Environment(
+					account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+					region=os.getenv('CDK_DEFAULT_REGION'))
+)
+
+# Security Group Stack
+security_group_stack = SecurityGroupStack(app, "SecurityGroupStack",
+				vpc=vpc_stack.vpc,
 				env=Environment(
 					account=os.getenv('CDK_DEFAULT_ACCOUNT'),
 					region=os.getenv('CDK_DEFAULT_REGION'))
